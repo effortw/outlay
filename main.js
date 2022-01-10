3d3b7406fd5a2ca26c0b18fe33b3892472fd7045
Effort workers <74310343+effortw@users.noreply.github.com>
2022-01-10 17:05:12 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+var config = {
+    apiKey: "AIzaSyCWGbJi9qeD7h6BPhAKBoWupHv21Tge3ck",
+  authDomain: "effort-5efac.firebaseapp.com",
+  databaseURL: "https://effort-5efac.firebaseio.com",
+  projectId: "effort-5efac",
+  storageBucket: "effort-5efac.appspot.com",
+  messagingSenderId: "771931121287",
+  appId: "1:771931121287:web:9fe9abdcab7c369f277a22",
+  measurementId: "G-GLD9CY5VPF"};
+firebase.initializeApp(config);
+
+  
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+function googleSignin() {
+   firebase.auth()
+   
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token);
+      console.log(user);
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      alert(error.code);
+      alert(error.message);
+   });
+}
